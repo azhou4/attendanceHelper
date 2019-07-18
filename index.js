2c019e9c689f16d2bdd08c92649e576a99e830af
jennygu <jennygu@amazon.com>
2019-07-18 13:21:50 -0700

    update with students

--- index.js
+++ index.js
@@ -3,6 +3,49 @@
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
 
+const listOfStudents = {
+    "Elena": {
+        "contact": {
+            "email": "jjgg411@gmail.com",
+            "phone": "7573104372"
+        },
+        "attendance": {
+            "06/03/2019": true,
+            "06/04/2019": false
+        }        
+    },
+    "Amy": {
+        "contact": {
+            "email": "jjgg411@gmail.com",
+            "phone": "7573104372"
+        },
+        "attendance": {
+            "06/03/2019": true,
+            "06/04/2019": true
+        } 
+    },
+    "Laurel": {
+        "contact": {
+            "email": "jjgg411@gmail.com",
+            "phone": "7573104372"
+        },
+        "attendance": {
+            "06/03/2019": false,
+            "06/04/2019": false
+        } 
+    },    
+    "Jenny": {
+        "contact": {
+            "email": "jjgg411@gmail.com",
+            "phone": "7573104372"
+        },
+        "attendance": {
+            "06/03/2019": false,
+            "06/04/2019": true
+        } 
+    }
+ } 
+
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
@@ -22,6 +65,16 @@ const SendReportIntentHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'SendReportIntent';
     },
     handle(handlerInput) {
+        var today = new Date();
+        var dd = String(today.getDate().padStart(2, '0'));
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        var yyyy = today.getFullYear();
+        var date_string = mm + '/' + dd + '/' + yyyy;
+        for (var student in listOfStudents) {
+            if (!student.attendance.date_string) {
+                
+            }
+        }
         const speechText = 'Report Sent';
         // code to send report
         return handlerInput.responseBuilder
